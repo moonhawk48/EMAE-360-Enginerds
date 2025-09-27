95641b36f5957bd925d136a85e54a1520e2f73af
lucianrex <lra31@case.edu>
2025-09-26 23:54:20 -0400

    Matlab code to link CEA to Excel

---
+++ Thermofluid/MATLAB/CEAproducts V1 in Sheet 'CEA Inputs +Outputs' Column Z.m
@@ -0,0 +1,23 @@
+function q = CEAProducts(b)
+AFR = b(1);
+rho = b(2);
+temp = b(3);
+mN2 = b(4);
+mO2 = b(5);
+mAr = b(6);
+mOctane = b(7);
+mHeptane = b(8);
+mEthanol = b(9);
+P = CEA('problem','uv','equilibrium','f/a',AFR,'eq','rho,kg/m3',rho,'reac', ...
+    'ox','N2','wt%',mN2,'t(k)',temp,'ox', ...
+    'O2','wt%',mO2,'t(k)',temp, ...
+    'ox','Ar','wt%',mAr,'t(k)',temp, ...
+    'fu','C8H18,isooctane','wt%',mOctane,'t(k)',temp, ...
+    'fu','C7H16,n-heptane','wt%',mHeptane,'t(k)',temp, ...
+    'fu','C2H5OH','wt%',mEthanol,'t(k)',temp, ...
+    'output','end');
+
+q{1} = P.output.products.'
+q{2} = P.output.fractions.'
+
+end
