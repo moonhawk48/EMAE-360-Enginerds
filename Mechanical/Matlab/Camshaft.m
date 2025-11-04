ce347364e098f8fe39f6464c2b206a29e549bf01
jme92 <jme92@case.edu>
2025-11-03 22:23:30 -0500

    Update Camshaft.m

--- Mechanical/Matlab/Camshaft.m
+++ Mechanical/Matlab/Camshaft.m
@@ -1,10 +1,10 @@
 clear; clc; close all;
 
 % Input parameters
-Lv_max = 16.9;              % Max valve lift [mm]
-beta = 90;                  % Lift angle [deg]
-rpm = 2500;                 % Camshaft speed [rev/min]
-omega = rpm * 2*pi/60;      % Angular speed [rad/s]
+Lv_max = 16.9; % Max valve lift [mm]
+beta = 90; % Lift angle [deg]
+rpm = 2500; % Camshaft speed [rev/min]
+omega = rpm * 2*pi/60; % Angular speed [rad/s]
 
 % Solving for 4-5-6-7 Polynomial Coefficients
 b = 1;
@@ -18,38 +18,37 @@ a7 = coefficients(4);
 
 % Normalized motion variable
 u = 0:0.001:1;
-theta = u * beta;           % Crank angle [deg]
+theta = u * beta; % Crank angle [deg]
 
 % Base equations
-S = a4*u.^4 + a5*u.^5 + a6*u.^6 + a7*u.^7;                        % Displacement [mm]
-dS_du = 4*a4*u.^3 + 5*a5*u.^4 + 6*a6*u.^5 + 7*a7*u.^6;            % First derivative
-d2S_du2 = 12*a4*u.^2 + 20*a5*u.^3 + 30*a6*u.^4 + 42*a7*u.^5;      % Second derivative
-d3S_du3 = 24*a4*u + 60*a5*u.^2 + 120*a6*u.^3 + 210*a7*u.^4;       % Third derivative
+S = a4*u.^4 + a5*u.^5 + a6*u.^6 + a7*u.^7; % Displacement [mm]
+dS_du = 4*a4*u.^3 + 5*a5*u.^4 + 6*a6*u.^5 + 7*a7*u.^6; % First derivative
+d2S_du2 = 12*a4*u.^2 + 20*a5*u.^3 + 30*a6*u.^4 + 42*a7*u.^5; % Second derivative
+d3S_du3 = 24*a4*u + 60*a5*u.^2 + 120*a6*u.^3 + 210*a7*u.^4; % Third derivative
 
-% Convert to real units (mm/s, mm/s², mm/s³)
-V = dS_du * (omega/beta*pi/180);             % Velocity [mm/s]
-A = d2S_du2 * (omega/beta*pi/180)^2;         % Acceleration [mm/s²]
-J = d3S_du3 * (omega/beta*pi/180)^3;         % Jerk [mm/s³]
+% Convert to units (mm/s, mm/s², mm/s³)
+V = dS_du * (omega/beta*pi/180); % Velocity [mm/s]
+A = d2S_du2 * (omega/beta*pi/180)^2; % Acceleration [mm/s²]
+J = d3S_du3 * (omega/beta*pi/180)^3; % Jerk [mm/s³]
 
-% --- Scale x-axis to degrees ---
+% Scale x-axis to degrees
 theta1 = u * 90;  % 0–90 degrees
 
-% --- Mirror motion to 90–180° ---
+% Mirror motion from 90–180°
 theta2 = 180 - fliplr(theta1);  % 90–180 degrees
 Lv2 = fliplr(S);
-Lv_dot2 = -fliplr(V);     % velocity reverses
-Lv_ddot2 = fliplr(A);     % acceleration same direction
-Lv_tdot2 = -fliplr(J);    % jerk reverses
+Lv_dot2 = -fliplr(V); % velocity reverses
+Lv_ddot2 = fliplr(A); % acceleration remains the same
+Lv_tdot2 = -fliplr(J); % jerk reverses
 
-% --- Combine lift + return ---
+% Combine lift + return
 theta = [theta1, theta2];
 S = [S, Lv2];
 V = [V, Lv_dot2];
 A = [A, Lv_ddot2];
 J = [J, Lv_tdot2];
 
-
-% --- Plot SVAJ ---
+% Plot SVAJ
 figure;
 sgtitle(sprintf('SVAJ Curves Using 4-5-6-7 Polynomials @ %d RPM', rpm));
 
@@ -76,3 +75,17 @@ plot(theta, J, 'LineWidth', 1.5);
 xlabel('Crank Angle θ [deg]');
 ylabel('J [mm/s³]');
 grid on;
+
+% Cam Profile Mapping
+R_b = 20; % Base circle radius [mm]
+theta = deg2rad(theta); % Crank Angle [rad]
+
+% Mapping Profile PROBABLY MISSING THE DWELL FROM 180 TO 360 DEGREES
+for i = 1:1:length(theta)
+    x(i) = (R_b + S(i))*cos(theta(i)) - V(i)*sin(theta(i)); % X Function
+    y(i) = (R_b + S(i))*sin(theta(i)) - V(i)*cos(theta(i)); % Y Function
+end
+
+% Plot Profile NOT WORKING
+figure(2);
+polarplot(x,y,'LineWidth',1.5);
