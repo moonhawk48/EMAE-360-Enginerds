15d0c1bd5d269c631d4c4e93c8250c9a794f23a0
jme92 <jme92@case.edu>
2025-11-04 22:29:52 -0500

    Update Camshaft.m

--- Mechanical/Matlab/Camshaft.m
+++ Mechanical/Matlab/Camshaft.m
@@ -22,9 +22,15 @@ theta = u * beta; % Crank angle [deg]
 
 % Base equations
 S = a4*u.^4 + a5*u.^5 + a6*u.^6 + a7*u.^7; % Displacement [mm]
+<<<<<<< Updated upstream
 dS_du = 4*a4*u.^3 + 5*a5*u.^4 + 6*a6*u.^5 + 7*a7*u.^6; % First derivative
 d2S_du2 = 12*a4*u.^2 + 20*a5*u.^3 + 30*a6*u.^4 + 42*a7*u.^5; % Second derivative
 d3S_du3 = 24*a4*u + 60*a5*u.^2 + 120*a6*u.^3 + 210*a7*u.^4; % Third derivative
+=======
+dS_du = 4*a4*u.^3 + 5*a5*u.^4 + 6*a6*u.^5 + 7*a7*u.^6; % Velocity
+d2S_du2 = 12*a4*u.^2 + 20*a5*u.^3 + 30*a6*u.^4 + 42*a7*u.^5; % Acceleration
+d3S_du3 = 24*a4*u + 60*a5*u.^2 + 120*a6*u.^3 + 210*a7*u.^4; % Jerk
+>>>>>>> Stashed changes
 
 % Convert to units (mm/s, mm/s², mm/s³)
 V = dS_du * (omega/beta*pi/180); % Velocity [mm/s]
@@ -41,7 +47,11 @@ Lv_dot2 = -fliplr(V); % velocity reverses
 Lv_ddot2 = fliplr(A); % acceleration remains the same
 Lv_tdot2 = -fliplr(J); % jerk reverses
 
+<<<<<<< Updated upstream
 % Combine lift + return
+=======
+% Combining lift + return
+>>>>>>> Stashed changes
 theta = [theta1, theta2];
 S = [S, Lv2];
 V = [V, Lv_dot2];
@@ -78,6 +88,7 @@ grid on;
 
 % Cam Profile Mapping
 R_b = 20; % Base circle radius [mm]
+<<<<<<< Updated upstream
 theta = deg2rad(theta); % Crank Angle [rad]
 
 % Mapping Profile PROBABLY MISSING THE DWELL FROM 180 TO 360 DEGREES
@@ -89,3 +100,27 @@ end
 % Plot Profile NOT WORKING
 figure(2);
 polarplot(x,y,'LineWidth',1.5);
+=======
+theta = [deg2rad(theta), linspace(180,360,2002)]; % Crank Angle [rad]
+S = [S, R_b.*ones(1,length(S))]; % Adding no lift from 180 to 360 degrees, only base radius???
+V = [V, zeros(1,length(V))]; % Adding zero velocity from 180 to 360 degrees
+
+% % Mapping Profile
+% for i = 1:1:length(theta)
+%     x(i) = (R_b + S(i))*cos(theta(i)) - V(i)*sin(theta(i)); % X Function
+%     y(i) = (R_b + S(i))*sin(theta(i)) - V(i)*cos(theta(i)); % Y Function
+% end
+
+figure(2);
+for i = 1:length(theta)
+    x(i) = (R_b + S(i))*cos(theta(i)) - V(i)*sin(theta(i)); % X Function
+    y(i) = (R_b + S(i))*sin(theta(i)) - V(i)*cos(theta(i)); % Y Function
+    plot(x(1:i),y(1:i))
+    pause(0.001)
+end
+
+% Plot Profile NOT WORKING, FIGURED OUT BECAUSE THE S Data before graphing
+% goes from 0mm to Lv_max, when it should go from R_b to R_b + Lv_max
+figure(2);
+plot(x,y,'LineWidth',1.5);
+>>>>>>> Stashed changes
